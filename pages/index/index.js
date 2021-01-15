93e1a9b4aa396697da3d0b35be248a835e5c3cb3
xulin <472086941@qq.com>
2021-01-15 22:23:23 +0800

    “首页开发完毕”

---
+++ pages/index/index.js
@@ -0,0 +1,113 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    kc:[
+      {id:1,url:"/images/kecheng1.png",title:"平面设计"},
+      {id:2,url:"/images/kecheng2.png",title:"室内"},
+      {id:3,url:"/images/kecheng3.png",title:"UI设计"},
+      {id:4,url:"/images/kecheng4.png",title:"前端开发"},
+      {id:5,url:"/images/kecheng5.png",title:"影视后期"}
+    ],
+    Students_Work:[
+      {
+        image:"/images/xszp1.jpg",
+        tit:"UI设计作品",       
+        info:"UI DESIGN WORKS"
+      },
+      {
+        image:"/images/xszp2.jpg",
+        tit:"平面设计作品",       
+        info:"GRAPHIC DESIGN WORKS"
+      },
+      {
+        image:"/images/xszp3.jpg",
+        tit:"电商设计作品",       
+        info:"E-COMMERCE DESIGN WORKS"
+      },
+      {
+        image:"/images/xszp4.jpg",
+        tit:"室内设计作品",       
+        info:"INTERIOT DESIGN WORKS"
+      },
+      {
+        image:"/images/xszp5.jpg",
+        tit:"影视后期作品",       
+        info:"POST-PRODUCTION WORKS"
+      },
+      {
+        image:"/images/xszp6.jpg",
+        tit:"三维仿真作品",       
+        info:"3D SIMULATION WORKS"
+      },
+      {
+        image:"/images/xszp7.jpg",
+        tit:"网页设计作品",       
+        info:"WEB DESIGN WORKS"
+      },
+      {
+        image:"/images/xszp8.jpg",
+        tit:"建筑景观作品",       
+        info:"BUILDING LANDSCAPE WORKS"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+   
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
